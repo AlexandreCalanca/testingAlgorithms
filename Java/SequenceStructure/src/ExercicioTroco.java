4702f75473a4b7b7d48bc3ce724d24b5b3c445d1
Alexandre Calanca <alexandrecalancaferreira@gmail.com>
2024-06-25 14:09:18 -0300

    Exercicio Troco

---
+++ Java/SequenceStructure/src/ExercicioTroco.java
@@ -0,0 +1,34 @@
+		//Bibliotecas
+import java.util.Scanner;
+import java.util.Locale;
+
+public class ExercicioTroco {
+	public static void main(String[] args) {
+		
+		//Variaveis
+		double precoUni, dinRec, troco, total;
+		int qnt;
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Preco unitario do produto: ");
+		precoUni = sc.nextDouble();
+		
+		System.out.print("Quantidade comprada: ");
+		qnt = sc.nextInt();
+		total = (double) (precoUni * qnt);
+		
+		System.out.print("Dinheiro recebido: ");
+		dinRec = sc.nextDouble();
+		
+		troco = (dinRec - total);
+		System.out.printf("Troco: %.2f", troco);	
+		
+		
+		
+		
+		sc.close();
+	}
+
+}
